d45183dcf2a6ed148c15f7ead9cc6d77630befe7
Mingjie LIAO <mliao0320@sina.com>
2021-06-27 10:16:59 +0800

    loop

--- src/main.sh
+++ src/main.sh
@@ -1,1 +1,11 @@
+# run init.edp to get the results of 0s-2s
+FreeFem++-nw init.edp
 
+# to write a loop
+counter=2
+while(( ${counter}<10))
+do
+    echo "Time: ${counter}"
+    FreeFem++-nw NS.edp
+    let "counter++"
+done
